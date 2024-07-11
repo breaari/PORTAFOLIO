5704551c30106c3d0e089aa3495cc1ad0629bd6b
Ariana <aru.brea@gmail.com>
2024-07-11 01:58:50 -0300

    b

--- src/View/landing.jsx
+++ src/View/landing.jsx
@@ -1,11 +1,9 @@
 import { Nav } from '../components/NavBar/Nav'
 import { Home } from '../components/Home/Home'
 import { AboutMe } from '../components/AboutMe/AboutMe'
-import { Resume } from '../components/Resume/Resume'
 import { Expertise } from '../components/Expertise/Expertise'
 import { Projects } from '../components/Projects/Projects'
 import { Contact } from '../components/Contact/Contact'
-import { Studies } from '../components/Studies/Studies'
 import { Footer } from "../components/Footer/Footer"
 
 export const Landing = () => {
@@ -15,8 +13,6 @@ export const Landing = () => {
             <Home></Home>
             <AboutMe></AboutMe>
             <Expertise></Expertise>
-            {/* <Resume></Resume>
-            <Studies></Studies> */}
             <Projects></Projects>
             <Contact></Contact>
             <Footer></Footer>
