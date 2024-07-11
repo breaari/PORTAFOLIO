bd13a31bcf5f444a2de8342493aacf8a80740134
Ariana <aru.brea@gmail.com>
2024-07-11 19:49:35 -0300

    m

--- src/components/Home/left.jsx
+++ src/components/Home/left.jsx
@@ -7,16 +7,16 @@ export const Left = () => {
     return (
         <div className="bg-azuloscuro text-blanco absolute left-0 top-[25%] rounded-tr-md rounded-br-md px-3 py-5 text-3xl mq980:text-2xl">
             <a href="https://wa.me/5491136002250" target="_blank" rel="noopener noreferrer">
-                <FaWhatsapp className="mb-2" />
+                <FaWhatsapp className="mb-4" />
             </a>
             <a href="https://github.com/breaari" target="_blank" rel="noopener noreferrer">
-                <FaGithub className="my-2"/>
+                <FaGithub className="my-4"/>
             </a>
             <a href="https://www.linkedin.com/in/ariana-brea" target="_blank" rel="noopener noreferrer">
-                <FaLinkedinIn className="my-2"/>
+                <FaLinkedinIn className="my-4"/>
             </a>
             <a href="mailto:aru.brea@gmail.com" target="_blank" rel="noopener noreferrer">
-                <MdOutlineMailOutline className="mt-2"/>
+                <MdOutlineMailOutline className="mt-4"/>
             </a>
         </div>
     )
