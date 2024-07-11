177d9017e2378c8b6bbd7ffa6f701ac05f9e648d
Ariana <aru.brea@gmail.com>
2024-07-11 03:20:24 -0300

    e

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -4,10 +4,10 @@ import avatar from "../../assets/avatar2.png"
 export const Contact = ()=>{
     return (
         <div id="contact" className="h-screen max-w-screen bg-gradient-to-b from-celeste to-azuloscuro dark:from-celeste dark:to-celesteclaro flex flex-row px-12 pt-12 mq980:flex-col mq980:px-7 mq980:pt-7">
-            <div className="w-[60%] flex flex-col justify-center items-center mq980:w-full mq980:h-[50%] mq980:items-end">
+            <div className="w-[60%] flex flex-col justify-center items-center mq980:w-full mq980:h-[50%] mq980:justify-end">
             <Formulario></Formulario>
             </div> 
-           <div className="w-[40%] flex flex-col items-center justify-center mq980:w-full mq980:h-[50%] mq980:items-start">
+           <div className="w-[40%] flex flex-col items-center justify-center mq980:w-full mq980:h-[50%] mq980:justify-center">
            <div className=" h-[500px] w-[500px] overflow-hidden mq980:w-[300px] mq980:h-[300px]">
             <img src= {avatar} className=" h-[500px] w-[500px] objet-cover mq980:w-[300px] mq980:h-[300px]"></img>
             </div>
